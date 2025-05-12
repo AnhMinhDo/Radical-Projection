c80a96aec028cab2f3a2b739b1dc1cbcbe0117ab
anhminh <ahd.minh@gmail.com>
2025-05-12 18:13:18 +0200

    feat: Main method for the application

--- src/main/java/RadicalProjectionMain/Main.java
+++ src/main/java/RadicalProjectionMain/Main.java
@@ -1,22 +1,25 @@
 package RadicalProjectionMain;
 
-import ij.IJ;
+import org.scijava.Context;
 import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import javax.swing.*;
-import java.awt.*;
-import com.formdev.flatlaf.themes.FlatMacLightLaf;
-import UIDesign.Radical_Projection_Tool;
+
+import UIComponents.Radical_Projection_Tool;
 
 @Plugin(type = Command.class, menuPath = "Plugins > Radical Projection")
 public class Main implements Command {
+
+    @Parameter
+    private Context context; // get context from current Fiji session
+
     @Override
     public void run() {
-        launchUI();
+        SwingUtilities.invokeLater(()-> launchUI());
     }
 
-    public static void launchUI(){
-        SwingUtilities.invokeLater(() -> {
+    public void launchUI(){
             String os = System.getProperty("os.name").toLowerCase();
             try {
                 if (os.contains("mac")) {
@@ -30,10 +33,9 @@ public class Main implements Command {
             JFrame frame = new JFrame("Radical_Projection_UI");
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-            Radical_Projection_Tool form = new Radical_Projection_Tool();
+            Radical_Projection_Tool form = new Radical_Projection_Tool(context);
             frame.setContentPane(form.getContentPane());
             frame.pack();
             frame.setVisible(true);
-        });
     }
 }
