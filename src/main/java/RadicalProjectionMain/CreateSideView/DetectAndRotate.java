be8ffcf95961b5b2f5b430f7d82e82d95cc0800b
anhminh <ahd.minh@gmail.com>
2025-05-13 11:55:07 +0200

    feat: detect the orientation of the stack and rotate to achieve the width > height requirement.

---
+++ src/main/java/RadicalProjectionMain/CreateSideView/DetectAndRotate.java
@@ -0,0 +1,31 @@
+package RadicalProjectionMain.CreateSideView;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ImageConverter;
+import ij.process.StackProcessor;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.real.FloatType;
+
+public class DetectAndRotate {
+    private String filePath;
+
+    public DetectAndRotate (String filePath){
+        this.filePath = filePath;
+    }
+    public Img<FloatType> process() {
+        ImagePlus inputImageStack = new ImagePlus(this.filePath);
+        if(inputImageStack.getWidth() > inputImageStack.getHeight()){
+        StackProcessor sp = new StackProcessor(inputImageStack.getStack(), null);
+        ImageStack rotatedStack = sp.rotateLeft();
+        inputImageStack.setStack(rotatedStack);
+        ImageConverter converter = new ImageConverter(inputImageStack);
+        converter.convertToGray32();
+        return ImagePlusAdapter.wrapImgPlus(inputImageStack);
+    } else {
+            return ImagePlusAdapter.wrapImgPlus(inputImageStack);
+        }
+    }
+}
