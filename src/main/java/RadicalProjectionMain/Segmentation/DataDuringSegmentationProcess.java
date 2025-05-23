68a94d98740e4f837cb0f895f92eb253a48000a3
anhminh <ahd.minh@gmail.com>
2025-05-23 07:38:59 +0200

    feat: data class for transfering the data between steps

---
+++ src/main/java/RadicalProjectionMain/Segmentation/DataDuringSegmentationProcess.java
@@ -0,0 +1,46 @@
+package RadicalProjectionMain.Segmentation;
+
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.real.FloatType;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class DataDuringSegmentationProcess {
+    private  int width;
+    private  int height;
+    private  int diameter;
+    private  RandomAccessibleInterval<FloatType> smoothStack;
+
+    public DataDuringSegmentationProcess(RandomAccessibleInterval<FloatType> smoothStack,
+                                         int width,
+                                         int height,
+                                         int diameter) {
+        this.smoothStack = smoothStack;
+        this.width = width;
+        this.height = height;
+        this.diameter = diameter;
+    }
+    public RandomAccessibleInterval<FloatType> getSmoothStack() {
+        return smoothStack;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+
+
+
+
+
+
+}
