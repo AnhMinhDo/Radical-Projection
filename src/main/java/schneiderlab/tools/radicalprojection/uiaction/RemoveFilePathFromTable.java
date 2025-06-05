7311bc57452ac6e8505bf58c376f5e15a1857440
anhminh <ahd.minh@gmail.com>
2025-06-05 19:06:48 +0200

    feat: actionListener to remove file path from table

---
+++ src/main/java/schneiderlab/tools/radicalprojection/uiaction/RemoveFilePathFromTable.java
@@ -0,0 +1,23 @@
+package schneiderlab.tools.radicalprojection.uiaction;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RemoveFilePathFromTable implements ActionListener {
+    private final JTable table;
+
+    public RemoveFilePathFromTable(JTable table) {
+        this.table = table;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow != -1) {
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            model.removeRow(selectedRow);
+        }
+    }
+}
