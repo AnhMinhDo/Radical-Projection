e7ddf34698734286b18a08cd7fb079fbdf174b6c
anhminh <ahd.minh@gmail.com>
2025-07-25 11:49:43 +0200

    refactor: ActionListener for button to start the conversion process

---
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/uiaction/BrowseButtonCZIToTif.java
@@ -0,0 +1,28 @@
+package schneiderlab.tools.radialprojection.controllers.uiaction;
+
+import schneiderlab.tools.radialprojection.views.userinterfacecomponents.Radical_Projection_Tool;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BrowseButtonCZIToTif implements ActionListener {
+    private JTextField textFieldFolderPath;
+    private JFrame parentFrame;
+
+    public BrowseButtonCZIToTif(JTextField textFieldFolderPath, JFrame parentFrame) {
+        this.textFieldFolderPath = textFieldFolderPath;
+        this.parentFrame = parentFrame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = chooser.showOpenDialog(parentFrame);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            textFieldFolderPath.setText(chooser.getSelectedFile().getAbsolutePath());
+    }
+}
+
+}
