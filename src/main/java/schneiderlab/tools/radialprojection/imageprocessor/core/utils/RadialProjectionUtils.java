508f563231310a93f457a8eeab1abb08e09933d8
anhminh <ahd.minh@gmail.com>
2025-08-15 12:05:53 +0200

    feat: util class to store frequently used static functions of the project

---
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/utils/RadialProjectionUtils.java
@@ -0,0 +1,15 @@
+package schneiderlab.tools.radialprojection.imageprocessor.core.utils;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class RadialProjectionUtils {
+
+    public static ArrayList<Point> deepCopyPoints(ArrayList<Point> points) {
+        ArrayList<Point> copy = new ArrayList<>();
+        for (Point p : points) {
+            copy.add(new Point(p.x, p.y));
+        }
+        return copy;
+    }
+}
