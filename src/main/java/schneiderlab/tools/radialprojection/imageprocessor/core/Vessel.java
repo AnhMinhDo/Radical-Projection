73cad74b9846f2a8014e89f356c6dcd30386c1ff
anhminh <ahd.minh@gmail.com>
2025-08-18 15:12:31 +0200

    feat: Vessel class to hold all the info of one vessel, allow for handling multiple Vessels

---
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/Vessel.java
@@ -0,0 +1,18 @@
+package schneiderlab.tools.radialprojection.imageprocessor.core;
+
+import ij.ImagePlus;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class Vessel {
+
+    private HashMap<Integer, Point> centroidMap;
+    private HashMap<Integer, Point> clickMap;
+    private HashMap<Integer, Integer> trueLabelMap;
+    private ImagePlus radialProjected;
+    private ImagePlus unrolled;
+
+
+
+}
