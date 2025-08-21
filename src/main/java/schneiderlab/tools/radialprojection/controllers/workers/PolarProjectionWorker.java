1566eb44d994129eb0c1e68f9013bd46e5871338
anhminh <ahd.minh@gmail.com>
2025-08-21 09:04:16 +0200

    refactor: remove hard coded centroid list and replace with dynamically Vessel Array, allow for dynamically number of vessels to be Polar Projected

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/PolarProjectionWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/PolarProjectionWorker.java
@@ -1,52 +1,45 @@
 package schneiderlab.tools.radialprojection.controllers.workers;
 
 import ij.ImagePlus;
+import schneiderlab.tools.radialprojection.imageprocessor.core.Vessel;
 import schneiderlab.tools.radialprojection.imageprocessor.core.polarprojection.PolarProjection;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class PolarProjectionWorker extends SwingWorker<Void, Void> {
     private final ImagePlus hybridStack;
     private final ImagePlus edgeBinaryMaskEdge;
-    private final ArrayList<Point> centroidListVessel1;
-    private final ArrayList<Point> centroidListVessel2;
-    private ImagePlus vessel1PolarProjection;
-    private ImagePlus vessel2PolarProjection;
+    private ArrayList<Vessel> vesselArrayList;
+    private ArrayList<ImagePlus> vesselPolarProjectionArrayList;
 
     public PolarProjectionWorker(ImagePlus hybridStack,
                                  ImagePlus edgeBinaryMaskEdge,
-                                 HashMap<Integer, ArrayList<Point>> centroidHashMap) {
+                                 ArrayList<Vessel> vesselArrayList) {
         this.hybridStack = hybridStack;
         this.edgeBinaryMaskEdge =edgeBinaryMaskEdge;
-        this.centroidListVessel1 = centroidHashMap.get(1);
-        this.centroidListVessel2 = centroidHashMap.get(2);
+        this.vesselArrayList= vesselArrayList;
     }
 
-    public ImagePlus getVessel1PolarProjection() {
-        return vessel1PolarProjection;
-    }
-
-    public ImagePlus getVessel2PolarProjection() {
-        return vessel2PolarProjection;
+    public ArrayList<ImagePlus> getVesselPolarProjectionArrayList() {
+        return vesselPolarProjectionArrayList;
     }
 
     @Override
     protected Void doInBackground() {
-        PolarProjection polarProjection1 = new PolarProjection(hybridStack,
-                edgeBinaryMaskEdge,
-                centroidListVessel1,
-                5 // 5 is considered adequately small
-                );
-        vessel1PolarProjection=polarProjection1.process();
-        PolarProjection polarProjection2 = new PolarProjection(hybridStack,
-                edgeBinaryMaskEdge,
-                centroidListVessel2,
-                5
-        );
-        vessel2PolarProjection=polarProjection2.process();
+        vesselPolarProjectionArrayList = new ArrayList<>(vesselArrayList.size());
+        for (int i = 0; i < vesselArrayList.size(); i++) {
+            PolarProjection polarProjection = new PolarProjection(hybridStack,
+                    edgeBinaryMaskEdge,
+                    vesselArrayList.get(i).getCentroidArrayList(),
+                    5 // 5 degree is considered adequately small angle
+            );
+            ImagePlus vesselPolarProjection=polarProjection.process();
+            String imageTitle = "Radial Projection Vessel " + (i + 1);
+            vesselPolarProjection.setTitle(imageTitle);
+            vesselPolarProjectionArrayList.add(vesselPolarProjection);
+        }
         return null;
     }
 }
