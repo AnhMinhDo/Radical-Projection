9aece481bf0a81c8513b90df9ea2a5843dafeb64
anhminh <ahd.minh@gmail.com>
2025-08-21 09:05:54 +0200

    refactor: set the capacity for the result before the copy loop. Ensuring that Arraylist allocates sufficient memory for the process

--- src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/utils/RadialProjectionUtils.java
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/utils/RadialProjectionUtils.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class RadialProjectionUtils {
 
     public static ArrayList<Point> deepCopyPoints(ArrayList<Point> points) {
-        ArrayList<Point> copy = new ArrayList<>();
+        ArrayList<Point> copy = new ArrayList<>(points.size());
         for (Point p : points) {
             copy.add(new Point(p.x, p.y));
         }
