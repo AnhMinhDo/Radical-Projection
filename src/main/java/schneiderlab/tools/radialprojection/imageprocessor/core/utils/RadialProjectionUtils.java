8b6fb92687e8444207faed8e8c8789e5994690da
anhminh <ahd.minh@gmail.com>
2025-08-27 16:48:17 +0200

    feat: add function to convert RandomAccessInterval to a new ImagePlus object ( not a wrapper of the RAI)

--- src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/utils/RadialProjectionUtils.java
+++ src/main/java/schneiderlab/tools/radialprojection/imageprocessor/core/utils/RadialProjectionUtils.java
@@ -1,5 +1,14 @@
 package schneiderlab.tools.radialprojection.imageprocessor.core.utils;
 
+import ij.ImagePlus;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -11,5 +20,19 @@ public class RadialProjectionUtils {
             copy.add(new Point(p.x, p.y));
         }
         return copy;
+    }
+
+    public static ImagePlus copyAndConvertRandomAccessIntervalToImagePlus(RandomAccessibleInterval<FloatType> input, String name){
+        // Create copy using cursors
+        Img<FloatType> copy = ArrayImgs.floats(Intervals.dimensionsAsLongArray(input));
+        net.imglib2.Cursor<FloatType> srcCursor = Views.flatIterable(input).cursor();
+        net.imglib2.Cursor<FloatType> dstCursor = copy.cursor();
+        while (srcCursor.hasNext()) {
+            dstCursor.next().set(srcCursor.next());
+        }
+        // Convert to ImagePlus
+        ImagePlus impFloat = ImageJFunctions.wrap(copy, name);
+        impFloat.resetDisplayRange();
+        return impFloat;
     }
 }
