9282cd9ebf60f4c6c0af27c88ea25b5a3eb9e5a6
anhminh <ahd.minh@gmail.com>
2025-09-04 23:54:49 +0200

    feat: worker now handles lignin and cellulose channels

--- src/main/java/schneiderlab/tools/radialprojection/controllers/workers/UnrollVesselWorker.java
+++ src/main/java/schneiderlab/tools/radialprojection/controllers/workers/UnrollVesselWorker.java
@@ -11,14 +11,20 @@ import java.util.HashMap;
 
 public class UnrollVesselWorker extends SwingWorker<Void, Void> {
     private final ImagePlus hybridStack;
+    private final ImagePlus ligninStack;
+    private final ImagePlus celluloseStack;
     private final ImagePlus edgeBinaryMaskEdge;
-    private ArrayList<Vessel> vesselArrayList;
+    private final ArrayList<Vessel> vesselArrayList;
     private ArrayList<ImagePlus> vesselUnrolledArrayList;
 
     public UnrollVesselWorker(ImagePlus hybridStack,
+                              ImagePlus celluloseStack,
+                              ImagePlus ligninStack,
                                  ImagePlus edgeBinaryMaskEdge,
                                  ArrayList<Vessel> vesselArrayList) {
         this.hybridStack = hybridStack;
+        this.ligninStack = ligninStack;
+        this.celluloseStack = celluloseStack;
         this.edgeBinaryMaskEdge =edgeBinaryMaskEdge;
         this.vesselArrayList= vesselArrayList;
     }
@@ -29,18 +35,49 @@ public class UnrollVesselWorker extends SwingWorker<Void, Void> {
 
     @Override
     protected Void doInBackground() {
-        vesselUnrolledArrayList = new ArrayList<>(vesselArrayList.size());
+        vesselUnrolledArrayList = new ArrayList<>(vesselArrayList.size()*3); // 3 for lignin channel, cellulose channel, hybrid
+        int currentProgress = 0;
+        int increment = (int) 100.0/(vesselArrayList.size()*3);
+        setProgress(currentProgress);
         for (int i = 0; i < vesselArrayList.size(); i++) {
-            UnrollSingleVessel unrolled = new UnrollSingleVessel(hybridStack,
+            // create objects for Unrolling class
+            UnrollSingleVessel unrolledLignin = new UnrollSingleVessel(ligninStack,
                     edgeBinaryMaskEdge,
                     vesselArrayList.get(i).getCentroidArrayList(),
-                    5
+                    5 // 5 degree is considered adequately small angle
             );
-            ImagePlus vesselUnrolled=unrolled.process();
-            String imageTitle = "Unrolled Vessel " + (i + 1);
-            vesselUnrolled.setTitle(imageTitle);
-            vesselUnrolledArrayList.add(vesselUnrolled);
-            vesselArrayList.get(i).setUnrolledVessel(vesselUnrolled);
+            UnrollSingleVessel unrolledCellulose = new UnrollSingleVessel(celluloseStack,
+                    edgeBinaryMaskEdge,
+                    vesselArrayList.get(i).getCentroidArrayList(),
+                    5 // 5 degree is considered adequately small angle
+            );
+            UnrollSingleVessel unrolledHybrid = new UnrollSingleVessel(hybridStack,
+                    edgeBinaryMaskEdge,
+                    vesselArrayList.get(i).getCentroidArrayList(),
+                    5 // 5 degree is considered adequately small angle
+            );
+            // start unrolling
+            ImagePlus vesselUnrolledHybrid=unrolledHybrid.process();
+            currentProgress= currentProgress+increment;
+            setProgress(currentProgress);
+            ImagePlus vesselUnrolledLignin=unrolledLignin.process();
+            currentProgress= currentProgress+increment;
+            setProgress(currentProgress);
+            ImagePlus vesselUnrolledCellulose=unrolledCellulose.process();
+            currentProgress= currentProgress+increment;
+            setProgress(currentProgress);
+            String imageTitleHybrid = "Unrolled Vessel " + (i + 1) + " Hybrid";
+            String imageTitleCellulose = "Unrolled Vessel " + (i + 1) + " Cellulose channel";
+            String imageTitleLignin = "Unrolled Vessel " + (i + 1) + " Lignin channel";
+            vesselUnrolledHybrid.setTitle(imageTitleHybrid);
+            vesselUnrolledLignin.setTitle(imageTitleLignin);
+            vesselUnrolledCellulose.setTitle(imageTitleCellulose);
+            vesselUnrolledArrayList.add(vesselUnrolledHybrid);
+            vesselUnrolledArrayList.add(vesselUnrolledLignin);
+            vesselUnrolledArrayList.add(vesselUnrolledCellulose);
+            vesselArrayList.get(i).setUnrolledVesselHybrid(vesselUnrolledHybrid.duplicate());
+            vesselArrayList.get(i).setUnrolledVesselLignin(vesselUnrolledLignin.duplicate());
+            vesselArrayList.get(i).setUnrolledVesselCellulose(vesselUnrolledCellulose.duplicate());
         }
 
 
